ec9f6a24408d2303a74c89d60a0972b707cdfead
ElenaStr13 <estrateluk@yahoo.com>
2024-03-17 20:58:29 +0200

    homework10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {Users} from "./components/Users";
+import {useState} from "react";
+import {UsersFc} from "./components/UsersFC";
 
 function App() {
-  return (
-    <div className="App">
-      Hello world world
+    const [flag, setFlag] = useState(true);
+    return (
+        <div>
+            {flag && <Users/>}
+            {/*{flag && <UsersFc/>}*/}
+            <button onClick={() => setFlag(prevState => !prevState)}>Hide/Show</button>
     </div>
   );
 }
